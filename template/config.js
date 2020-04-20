65cd97ec090fb0d3b47049a561792080e28739c1
tingtien <tianting@cqfmbank.com>
2020-04-20 22:09:47 +0800

    updated

--- template/config.js
+++ template/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 3000,
-    domain: "http://localhost:3000",
-    staticURL: "//localhost:3000",
-    globalJs: {
+    URL: "http://localhost:3000",
+    config: {
+        staticURL: "//localhost:3000",
         reactRoute: "",
         api: "http://localhost:3000"
     }
