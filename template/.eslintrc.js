0314334d7c3c9d8365fad0b96aeda3f9cbe40585
tingtien <tianting@cqfmbank.com>
2022-05-23 09:30:25 +0800

    packages is udpated

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,34 +1,45 @@
 module.exports = {
     parser: "babel-eslint",
     parserOptions: {
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true
-        },
-        sourceType: "module"
+      ecmaFeatures: {
+        experimentalObjectRestSpread: true,
+        jsx: true
+      },
+      sourceType: "module"
     },
     extends: [
-        "standard",
-        "plugin:prettier/recommended",
-        "plugin:react/recommended"
+      "standard",
+      "plugin:prettier/recommended",
+      "plugin:react/recommended",
+      "plugin:import/recommended"
     ],
     plugins: ["react", "prettier"],
     rules: {
-        "prettier/prettier": 2,
-        "react/prop-types": 0,
-        "react/display-name": 0,
-        "eqeqeq": 0,
-        // "no-undef": 0,
-        "camelcase": 0,
-        "no-unused-expressions": 0
+      "prettier/prettier": 2,
+      "react/prop-types": 0,
+      eqeqeq: 0,
+      camelcase: 0,
+      "standard/no-callback-literal": 0,
+      "import/no-named-as-default-member": 0
     },
     globals: {
-        MI: "readonly",
-        routes: "readonly"
+      MI: "readonly",
+      router: "readonly"
     },
     settings: {
-        "react": {
-            "version": "detect", // React version. "detect" automatically picks the version you 
+      "react": {
+        "version": "detect"
+      },
+      "import/resolver": {
+        webpack: {
+          config: {
+            resolve: {
+              alias: {
+                "@common": require("path").join(__dirname, "app/web/common")
+              }
+            }
+          }
         }
+      }
     }
-};
+  };
